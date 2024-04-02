3c81b77b6cb66f99b2297929fdabaa77d7f86080
pedrosh101 <pedro.sh101@gmail.com>
2024-04-02 14:58:11 -0300

    oldLogoandText

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,28 +14,28 @@ export default function Home() {
           <div className="sm:w-2/5 justify-end flex flex-col mb-4 gap-5">
             <h1 className="text-3xl">Viggiani Arquitetura</h1>
             <div className="bg-black h-0.2"></div>
-            <h1>
-              No final do ano de 1980, Ricardo Viggiani concluiu o curso de
-              arquitetura na Universidade Mackenzie em São Paulo. Já trabalhava
-              como estagiário no Condephaat, onde permaneceu, agora efetivado
-              como arquiteto, até maio de 1981, quando surgiu a oportunidade de
-              trabalhar em Ubatuba. Desde então, reside lá, dedicando-se aos
-              projetos e obras, especialmente de casas na praia.<br></br> Ao
-              longo de 43 anos, Ricardo e sua equipe desenvolveram vários
-              projetos para pessoas que optaram por morar ou passar temporadas
-              próximas ao mar.<br></br> Seu trabalho consiste em orientar os
-              clientes desde o melhor aproveitamento das áreas, seguindo a
-              legislação municipal e estadual, até a conclusão da obra e
-              obtenção do habite-se, passando pelo acompanhamento de todas as
-              etapas da construção.<br></br>
-              Conceitos como uso de energia solar e fotovoltaica,
-              reaproveitamento das águas, biodigestores e uso sustentável dos
-              materiais, incluindo madeiras renováveis, são priorizados em seus
-              projetos, desenvolvidos em colaboração com os clientes, seja em
-              reformas ou novas construções, sempre visando uma melhor qualidade
-              de vida. Com essa filosofia, Ricardo Viggiani e sua equipe têm
-              mais de 250 obras entregues nas diversas praias do litoral norte.
-              Alguns desses projetos podem ser visualizados aqui, ou em seu
+            <h1 className="font-sans text-base">
+              No final do ano de 1980 terminei o curso de arquitetura na
+              Universidade Mackenzie em Sao Paulo. Ja trabalhava como estagiário
+              no Condephaat, onde permaneci, ja efetivado como arquiteto, até
+              maio de 1981, quando surgiu a oportunidade de trabalhar em
+              Ubatuba. E aqui resido até hoje, me dedicando aos projetos e
+              obras, especialmente de casas na praia.<br></br>
+              Nesses 43 anos desenvolvemos vários projetos para as pessoas que
+              optaram por morar ou passar temporadas próximas ao mar. Nosso
+              trabalho consiste em orientar os clientes desde o melhor
+              aproveitamento das suas áreas, seguindo a legislação municipal e
+              estadual, até a conclusão da sua obra e obtenção do habite-se,
+              passando pelo acompanhamento de todas etapas da sua construção.
+              <br></br>
+              Uso da energia solar e fotovoltaica, reaproveitamento das águas,
+              biodigestores, uso sustentável dos materiais, madeiras
+              renováveis…….são conceitos que procuramos desenvolver em nossos
+              projetos, junto com nossos clientes, seja em reformas ou em novas
+              construções, objetivando sempre uma melhor qualidade de vida. E
+              com essa filosofia, temos hoje mais de 250 obras entregues nas
+              diversas praias do litoral norte.<br></br> Algumas delas voce pode ver
+              aqui, ou em nosso
               <Link
                 href="www.instagram.com/viggianiarquitetura"
                 target="_blank"
@@ -46,6 +46,7 @@ export default function Home() {
               </Link>
               .
             </h1>
+            <h1 className="text-end font-sans text-base">Ricardo Viggiani</h1>
           </div>
           <div className="sm:w-3/5 relative overflow-hidden">
             <Image
